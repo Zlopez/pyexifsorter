7e7f98f2fc36fe495072434a9d16d3fda21b7a51
AsciiWolf <mail@asciiwolf.com>
2017-05-08 21:08:22 +0200

    Drop now unused logging script

--- src/cus_logging.py
+++
@@ -1,5 +0,0 @@
-import logging
-
-LOG_LEVEL = logging.DEBUG
-
-logging.basicConfig(level=LOG_LEVEL, format='%(asctime)-15s %(filename)s/%(funcName)s %(levelname)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S')
